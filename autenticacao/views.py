9162d59bf4da92f70d99e726c3c765fd2fcb10c8
Arthur Silva <arthursns@gmail.com>
2022-01-11 23:39:33 -0300

    Criação dos diretórios

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    return HttpResponse('teste')
+    return render(request, 'cadastro.html')
+
+def logar(request):
+    return HttpResponse('Logar')
